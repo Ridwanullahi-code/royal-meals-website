374510e45db1764f71ca55a41be2c8d3534a5042
kennankole <kennankole@gmail.com>
2022-10-26 17:03:05 +0300

    Saving likes with the involvement API

--- src/index.js
+++ src/index.js
@@ -1,7 +1,4 @@
-/* eslint-disable no-console */
 import fetchImagesApi from './modules/fetch-images.js';
-import clikcCount from './modules/involve.js';
 import './index.css';
 
 fetchImagesApi();
-clikcCount();
--- src/modules/involve.js
+++
@@ -1,4 +0,0 @@
-/* eslint-disable no-undef */
-/* eslint-disable no-unused-vars */
-/* eslint-disable func-names */
-/* eslint-disable no-console */
