0bfdf1f64b24b4bf134a254309b08055660c0368
ridwanullahi <ridwanullahiajayi01@gmail.com.com>
2022-10-27 00:51:49 +0100

    last update

--- src/index.js
+++ src/index.js
@@ -1,16 +1,11 @@
-<<<<<<< HEAD
 import '../css/index.css';
 import '../css/popup.css';
 import Main from './modules/home.js';
 import Action from './modules/action.js';
+import fetchImagesApi from "./modules/fetch-images.js";
 
 Main.main();
-Action.comment();
-Action.reservate();
-Action.removePop();
-=======
-import fetchImagesApi from './modules/fetch-images.js';
-import './index.css';
-
-fetchImagesApi();
->>>>>>> e4a3e8991e7640ec5cad8ca992e8cb91682aaebc
+// Action.comment();
+// Action.reservate();
+// Action.removePop();
+// fetchImagesApi()
--- src/modules/home.js
+++ src/modules/home.js
@@ -1,30 +1,27 @@
-<<<<<<< HEAD
 export default class Main {
   static main() {
     const mainPage = document.querySelector('.meals');
     for (let i = 0; i < 6; i += 1) {
       mainPage.innerHTML += `
-<div class="image-container">
-<img src="meals.jpg" alt="meal-image" height="70%" width="70%">
-<div class="likes">
-<p>Serving two plages</p>
-<i class="fa-regular fa-heart"></i>
-</div>
-<div class="likes-counter">
-<p>2 likes</p>
-</div>
-<ul class="users-sections">
-<li class="comments">
-<a href="#" class="comment-btn">Commnets</a>
-</li>
-<li class="reservations">
-<a class="reserve-btn" href="#">Reservations</a>
-</li>
-</ul>
-</div>
-`;
-    }
-  }
+    <div class="image-container">
+    <img src="meals.jpg" alt="meal-image" height="70%" width="70%">
+    <div class="likes">
+    <p>Serving two plages</p>
+    <i class="fa-regular fa-heart"></i>
+      </div>
+    <div class="likes-counter">
+    <p>2 likes</p>
+    </div>
+    <ul class="users-sections">
+    <li class="comments">
+    <a href="#" class="comment-btn">Commnets</a>
+    </li>
+    <li class="reservations">
+    <a class="reserve-btn" href="#">Reservations</a>
+    </li>
+    </ul>
+    </div>
+    `;
+        }
+      }
 }
-=======
->>>>>>> e4a3e8991e7640ec5cad8ca992e8cb91682aaebc
