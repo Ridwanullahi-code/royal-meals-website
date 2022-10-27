01a589d084e99340171568233ccf63a3c99b26ca
kennankole <kennankole@gmail.com>
2022-10-27 12:33:03 +0300

    Resolved merge conflict between likes-API branch and popup branch.

--- src/modules/home.js
+++ src/modules/home.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-tabs */
 // eslint-disable-next-line no-tabs
+import { getLikes, likePost } from './likes.js';
 
 export default class Main {
   static main() {
@@ -10,26 +11,30 @@ export default class Main {
       .then((data) => {
         data.meals.forEach((item) => {
           mainPage.innerHTML += `
-				<div class="image-container">
+
+				<div class="image-container" id="${item.idMeal}">
 					<img class="image" src="${item.strMealThumb}" alt="meal-image" height="70%" width="70%">
 					<div class="likes">
-						<p>Serving two plages</p>
-						<i class="fa-regular fa-heart"></i>
-					</div>
-					<div class="likes-counter">
-						<p>2 likes</p>
-					</div>
+						<p>${item.strMeal}</p>
+							<div class="like-container">
+						<button class="likes-btn">
+							<i class="fa-regular fa-heart" id="hearts"></i>
+						</button>
+						<div class="span">
+							<span class="count-text">0</span>
+							<span>likes</span>
+						</div>
+					</di>
 					<ul class="users-sections">
 						<li class="comments">
 							<a href="#" class="comment-btn get-img">Commnets</a>
 						</li>
-					<li class="reservations">
-						<a href="#" class="reserve-btn get-img">Reservations</a>
-					</li>
 					</ul>
     	</div>
-    `;
+   		 `;
         });
+        getLikes();
+        likePost();
       });
   }
 }
