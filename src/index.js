e835047b868034c2cabe2e5badb1213d09d7bc36
ridwanullahi <ridwanullahiajayi01@gmail.com.com>
2022-10-27 17:24:57 +0100

    last update

--- src/index.js
+++ src/index.js
@@ -2,9 +2,7 @@ import "../css/index.css";
 import "../css/popup.css";
 import Main from "./modules/home.js";
 import Action from "./modules/action.js";
-import { test } from "./modules/counter.js";
 
 Main.main();
 Action.comment();
 Action.removePop();
-test();
